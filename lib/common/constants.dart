d1d36d7263014755df74c404331ada0ac0aa0822
chinnathambi0725@gmail.com <you@example.com>
2024-08-28 14:41:36 +0530

    first commit

---
+++ lib/common/constants.dart
@@ -0,0 +1,9 @@
+// ignore_for_file: non_constant_identifier_names
+
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+class Constants {
+  static String API_BASE_URL = '${dotenv.env['API_BASE_URL']}';
+  static String API_KEY = '${dotenv.env['API_KEY']}';
+  static String API_PREFIX = '${dotenv.env['API_PREFIX']}';
+}
